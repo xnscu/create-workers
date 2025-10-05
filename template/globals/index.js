bd9cc5cfd56c33909168196515e42fdf85d46c00
xiangnan <280145668@qq.com>
2025-10-06 00:38:31 +0800

    useFetch

--- template/globals/index.js
+++ template/globals/index.js
@@ -1,6 +1,5 @@
 export { Model } from 'xodel/lib/model.mjs'
 export * as utils from '~/lib/utils.mjs'
 export { Http, usePost, useGet } from '~/lib/http'
-export { default as Notice } from '~/lib/Notice'
 export { get, post, default as apiFetch } from '~/lib/apiFetch.mjs'
 export const log = console.log
--- template/src/views/index.vue
+++ template/src/views/index.vue
@@ -1,6 +1,35 @@
-<script setup></script>
+<script setup>
+import { useFetch } from '@vueuse/core'
+
+const { data, isFetching, error, execute } = useFetch('/api/hello', {
+  immediate: false,
+})
+</script>
 <template>
+  <h1>Good! Your cloudflare project is ready!</h1>
   <GlobalHello />
   <LocalHello />
-  <h1>This is an index page</h1>
+
+  <div style="margin-top: 20px">
+    <button @click="execute" :disabled="isFetching">
+      {{ isFetching ? 'fetching...' : 'request /hello' }}
+    </button>
+
+    <div
+      v-if="error"
+      style="margin-top: 10px; padding: 10px; border: 1px solid #ff6b6b; background-color: #ffe0e0"
+    >
+      <strong>error:</strong>
+      <pre>{{ error }}</pre>
+    </div>
+
+    <div
+      v-if="data"
+      style="margin-top: 10px; padding: 10px; border: 1px solid #ccc; background-color: #f9f9f9"
+    >
+      <pre>{{ data }}</pre>
+    </div>
+  </div>
 </template>
+
+<style scoped></style>
