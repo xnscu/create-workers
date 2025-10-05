3286876cd910b049046f0ca6465b5ca8255c7326
xiangnan <280145668@qq.com>
2025-10-06 00:10:08 +0800

    update slogan

--- utils/banner.cjs
+++ utils/banner.cjs
@@ -2,6 +2,6 @@ const s = require('gradient-string')([
   { color: '#42d392', pos: 0 },
   { color: '#42d392', pos: 0.1 },
   { color: '#647eff', pos: 1 },
-])('create-workers - Start an opinioned vite project')
+])('create-workers - Start an opinioned cloudflare workers + pages project with vue 3 + vite')
 
 console.log(JSON.stringify(s))
